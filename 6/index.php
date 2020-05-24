1f8fcec069718abfda09b6857d754748c063e483
Alexandra Borchakovskaya <borch.alex@yandex.ru>
2020-05-25 02:11:17 +0300

    Добавила 6 задание

---
+++ 6/index.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>Задание 6</title>
+</head>
+<body>
+
+
+<!-- PHP CODE -->
+<?php
+
+if (empty($_SERVER['PHP_AUTH_USER']) ||
+    empty($_SERVER['PHP_AUTH_PW']) ||
+    $_SERVER['PHP_AUTH_USER'] != 'admin' ||
+    md5($_SERVER['PHP_AUTH_PW']) != md5('admin')) {
+    header('HTTP/1.1 401 Unanthorized');
+    header('WWW-Authenticate: Basic realm="web6"');
+    print('<h1>401 Требуется авторизация. Пожалуйста, обновите страницу.</h1>');
+    exit();
+}
+
+$db_user = 'u16852';
+$db_pass = '7594281';
+
+$db = new PDO('mysql:host=localhost;dbname=u16852', $db_user, $db_pass, array(PDO::ATTR_PERSISTENT => true));
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  try {
+    $stmt = $db->prepare('DELETE FROM web5 WHERE login = ?');
+    $stmt->execute(array($_POST['delete']));
+  } catch (PDOException $e) {
+    echo 'Ошибка: ' . $e->getMessage();
+  }
+  header('Location: ./');
+}
+
+try {
+  $stmt = $db->query('SELECT * FROM web5');
+
+?>
+<!-- ./ PHP CODE -->
+
+
+
+  <div class="table-responsive">
+    <form method="POST">
+      <table class="table table-striped table-bordered table-hover table-sm">
+        <thead class="thead-dark">
+          <tr>
+            <th>ID</th>
+            <th>Логин</th>
+            <th>Пароль</th>
+            <th>Имя</th>
+            <th>Email</th>
+            <th>Год гождения</th>
+            <th>Пол</th>
+            <th>Кол-во конеч.</th>
+            <th>Способности</th>
+            <th>Биография</th>
+            <th>Удалить</th>
+          </tr>
+        </thead>
+        <tbody>
+
+
+<?php
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+  print('<tr>');
+  foreach ($row as $cell) {
+    print('<td>' . strip_tags($cell) . '</td>');
+  }
+  print('<td><button class="btn btn-outline-danger" name="delete" type="submit" value="'
+  . strip_tags($row['login'])
+  . '">x</button></td>');
+  print('</tr>');
+}
+?>
+
+
+        </tbody>
+      </table>
+    </form>
+  </div>
+
+<?php
+} catch (PDOException $e) {
+    echo 'Ошибка: ' . $e->getMessage();
+    exit();
+}
+?>
+
+</body>
+</html>
